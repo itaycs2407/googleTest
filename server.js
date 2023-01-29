333781e0b6032198f4aaa1427482064102fccea5
Itay Cohen <itaycs2407@gmail.com>
2023-01-29 20:40:17 +0200

    init

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.get("/test_get_method", (req, res) => {
+  console.log("get\n");
+  res.status(200).send("response from get");
+});
+
+app.post("/test_post_method", (req, res) => {
+  console.log("post\n");
+  res.status(201).send({ message: "response from post" });
+});
+
+app.put("/test_put_method", (req, res) => {
+  console.log("put\n");
+  res.status(201).send({ message: "response from put" });
+});
+
+app.delete("/test_delete_method", (req, res) => {
+  console.log("delete\n");
+  res.status(201).send({ message: "response from delete" });
+});
+
+const server = app.listen(9589, () => {
+  console.log("Server listening on port 9589...\n");
+});
